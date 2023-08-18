5d22bcddb017a3cbb1e7955d40064a0fc09db1d1
Shariful Islam <70468977+Sharif355@users.noreply.github.com>
2023-08-18 21:44:20 +0600

    changes in js code

--- script.js
+++ script.js
@@ -1,76 +1,58 @@
 // Triangle section
 document.getElementById('btn-triangle').addEventListener('click', function () {
-    const inputValueWidth = document.getElementById('input-width');
-    const inputWidth = inputValueWidth.value;
-    const inputValueHeight = document.getElementById('input-height');
-    const inputHeight = inputValueHeight.value;
-    const textField = document.getElementById('triangle-area')
-    textField.innerText = 0.5 * inputWidth * inputHeight;
-    inputValueWidth.value = '';
-    inputValueHeight.value = '';
-
+    const base = getInputValueById('input-width');
+    const height = getInputValueById('input-height');
+    const area = 0.5 * base * height;
+    setTextFieldById('triangle-area', area)
 })
 // Rectangle section
 document.getElementById('btn-rectangle').addEventListener('click', function () {
-    const inputValueWidth = document.getElementById('input-rec-width');
-    const inputWidth = inputValueWidth.value;
-    const inputValueHeight = document.getElementById('input-rec-length');
-    const inputHeight = inputValueHeight.value;
-    const textField = document.getElementById('rectangle-area')
-    textField.innerText = inputWidth * inputHeight;
-    inputValueWidth.value = '';
-    inputValueHeight.value = '';
-
+    const base = getInputValueById('input-rec-width');
+    const length = getInputValueById('input-rec-length');
+    const area = base * length;
+    setTextFieldById('rectangle-area', area)
 })
 // Parallelogram Section
 document.getElementById('btn-parallelogram').addEventListener('click', function () {
-    const inputValueWidth = document.getElementById('input-par-width');
-    const inputWidth = inputValueWidth.value;
-    const inputValueHeight = document.getElementById('input-par-height');
-    const inputHeight = inputValueHeight.value;
-    const textField = document.getElementById('parallelogram-area')
-    textField.innerText = inputWidth * inputHeight;
-    inputValueWidth.value = '';
-    inputValueHeight.value = '';
+    const base = getInputValueById('input-par-width');
+    const height = getInputValueById('input-par-height');
+    const area = base * height;
+    setTextFieldById('parallelogram-area', area)
+
 
 })
 // Rhombus section
 document.getElementById('btn-rhombus').addEventListener('click', function () {
-    const inputValueWidth = document.getElementById('input-rho-value1');
-    const inputWidth = inputValueWidth.value;
-    const inputValueHeight = document.getElementById('input-rho-value2');
-    const inputHeight = inputValueHeight.value;
-    const textField = document.getElementById('rhombus-area')
-    textField.innerText = 0.5 * inputWidth * inputHeight;
-    inputValueWidth.value = '';
-    inputValueHeight.value = '';
-
+    const diagonal1 = getInputValueById('input-rho-value1');
+    const diagonal2 = getInputValueById('input-rho-value2');
+    const area = 0.5 * diagonal1 * diagonal2;
+    setTextFieldById('rhombus-area', area)
 })
 // Pentagon section
 document.getElementById('btn-pentagon').addEventListener('click', function () {
-    const inputValueWidth = document.getElementById('input-p-value');
-    const inputWidth = inputValueWidth.value;
-    const inputValueHeight = document.getElementById('input-b-value');
-    const inputHeight = inputValueHeight.value;
-    const textField = document.getElementById('pentagon-area')
-    textField.innerText = 0.5 * inputWidth * inputHeight;
-    inputValueWidth.value = '';
-    inputValueHeight.value = '';
+    const perimeter = getInputValueById('input-p');
+    const apothem = getInputValueById('input-b');
+    const area = 0.5 * perimeter * apothem;
+    setTextFieldById('pentagon-area', area)
 
 })
 //Ellipse section
 document.getElementById('btn-ellipse').addEventListener('click', function () {
-    const inputValueWidth = document.getElementById('input-ellipse-1');
-    const inputWidth = inputValueWidth.value;
-    const inputValueHeight = document.getElementById('input-ellipse-2');
-    const inputHeight = inputValueHeight.value
-    const textField = document.getElementById('ellipse-area')
-    textField.innerText = 3.1416 * inputWidth * inputHeight;
-    inputValueWidth.value = '';
-    inputValueHeight.value = '';
+    const axisA = getInputValueById('input-ellipse-1');
+    const axisB = getInputValueById('input-ellipse-2');
+    const area = 3.1416 * axisA * axisB;
+    setTextFieldById('ellipse-area', area);
 })
 
-
+//reusable function => to reduce code repetition
 function getInputValueById(inputId) {
-    const valueById = document.getElementById('inputId')
+    const valueById = document.getElementById(inputId)
+    const inputValue = valueById.value;
+    const value = parseFloat(inputValue);
+    return value;
+}
+
+function setTextFieldById(textId, area) {
+    const textFieldById = document.getElementById(textId)
+    textFieldById.innerText = area;
 }
